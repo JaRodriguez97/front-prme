56d5477f63a5831e3f6834c5146174f86457c183
JaRodriguez97 <cristi542012@gmail.com>
2024-05-26 17:57:21 -0500

    load 2.0

--- src/app/modules/Landing/landing.component.ts
+++ src/app/modules/Landing/landing.component.ts
@@ -75,7 +75,7 @@ export class LandingComponent implements OnInit {
       this.publicService.medium_load = true;
 
       setTimeout(() => (this.publicService.load = false), 2200);
-    }, 1000);
+    }, 10000);
   }
 
   prevBtn() {
